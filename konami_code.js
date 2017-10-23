ec80eb6c997a34f0312e98afd1515c126b43dee7
Man Hong Lee <manhong323@gmail.com>
2017-10-23 21:01:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  let html = document.querySelector('body');
-  html.addEventListener('keydown', onKeyDownHandler);
+  let body = document.querySelector('body');
+  body.addEventListener('keydown', onKeyDownHandler);
 }
 
 init();
